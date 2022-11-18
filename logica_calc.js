4336b07735e9208b5abeaed51f3b927b52aea9a4
ROBERTOCABALLEROCUBERO <91220443+ROBERTOCABALLEROCUBERO@users.noreply.github.com>
2022-11-18 03:30:21 +0100

    update

---
+++ logica_calc.js
@@ -0,0 +1,11 @@
+document.addEventListener('load', function() {
+    let resultado = document.getElementById("resultado");
+    document.getElementById("9").addEventListener("click", () => {
+
+        resultado = 9;
+
+
+
+    });
+
+});
