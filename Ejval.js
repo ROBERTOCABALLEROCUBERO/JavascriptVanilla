d68d06ee46a1fd6d3e31c4b4a322f8d861dc32a0
ROBERTOCABALLEROCUBERO <91220443+ROBERTOCABALLEROCUBERO@users.noreply.github.com>
2022-11-24 19:26:58 +0100

    Validaciones

--- Ejval.js
+++ Ejval.js
@@ -1,35 +1,35 @@
-window.addEventListener('load', () => (document.getElementById('comprueba').addEventListener('click', () => 
-{
-let formularios = document.getElementsByTagName("input");
+window.addEventListener("load", () =>
+  document.getElementById("comprueba").addEventListener("click", () => {
+    let formularios = document.getElementsByTagName("input");
 
-for (let index = 0; index < formularios.length; index++) {
-    if (index == 0) {
-        document.getElementById('username').innerHTML = inputName.tooShort;
+    document.getElementById("respuesta0").innerHTML =
+      formularios[0].validationMessage;
+
+    if (
+      (document.getElementById("respuesta1").innerHTML =
+        formularios[1].checkValidity() == false)
+    ) {
+      document.getElementById("respuesta1").textContent =
+        "El email no es válido";
+    } else {
+      document.getElementById("respuesta1").textContent = "Esta bien";
     }
-    if (index == 1) {
-        
+    document.getElementById("respuesta2").innerHTML =
+      formularios[2].validationMessage;
+
+    if (
+      document.getElementById("pass").value ==
+      document.getElementById("passc").value
+    ) {
+      document.getElementById("respuesta3").textContent = "Esta bien";
+    } else {
+      if (document.getElementById("passc").value == "") {
+        document.getElementById("respuesta3").innerHTML =
+        formularios[3].validationMessage;
+      } else {
+        document.getElementById("respuesta3").textContent =
+          "La contraseña no coincide";
+      }
     }
-    if (index == 2) {
-        
-    }
-    if (index == 3) {
-        
-    }
-    
-}
-
-
-document.getElementById('error').innerHTML = inputName.validationMessage;
-document.getElementById('checkValidity').innerHTML = inputName.checkValidity();
-document.getElementById('validationMessage').innerHTML = inputName.validationMessage;
-document.getElementById('valueMissing').innerHTML = inputName.validity.valueMissing;
-document.getElementById('tooshort').innerHTML = inputName.validity.tooShort;
-document.getElementById('tooLong').innerHTML = inputName.validity.tooLong;
-
-
-
-
-}
-
-
-)))
+  })
+);
